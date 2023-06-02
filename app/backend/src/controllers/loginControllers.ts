e3637aadc2aafdc12d631a382cdeb082774524e0
EduardoL-s <eduardoluizmm@hotmail.com>
2023-06-02 18:55:37 -0300

    [Requisito 10] validações básicas de login

--- app/backend/src/controllers/loginControllers.ts
+++ app/backend/src/controllers/loginControllers.ts
@@ -2,8 +2,8 @@ import { Request, Response } from 'express';
 import loginService from '../services/loginService';
 
 async function login(req: Request, res: Response) {
-  const { email } = req.body;
-  const result = await loginService.loginAuthenticate(email);
+  const { email, password } = req.body;
+  const result = await loginService.loginAuthenticate(email, password);
   return res.status(result.status).json(result.message);
 }
 
--- app/backend/src/services/loginService.ts
+++ app/backend/src/services/loginService.ts
@@ -1,16 +1,24 @@
-// import { compare } from 'bcryptjs';
+import { compare } from 'bcryptjs';
 import UserModel from '../database/models/UserModel';
 import { generateToken } from '../utils/auth';
 
-async function loginAuthenticate(email: string) {
+async function loginAuthenticate(email: string, password: string) {
   const user = await UserModel.findOne({ where: { email } });
 
-  if (user) {
-    const tokenGen = generateToken(user.id, user.role);
-    return { status: 200, message: { token: tokenGen } };
+  const formatEmail = /\S+@\S+\.\S+/;
+
+  if (!formatEmail.test(email) || !user) {
+    return { status: 401, message: { message: 'Invalid email or password' } };
   }
 
-  return { status: 200, message: { message: 'algo de errado' } };
+  const passwordVerify = await compare(password, user.password);
+
+  if (password.length < 6 || !passwordVerify) {
+    return { status: 401, message: { message: 'Invalid email or password' } };
+  }
+
+  const tokenGen = generateToken(user.id, user.role);
+  return { status: 200, message: { token: tokenGen } };
 }
 
 export default { loginAuthenticate };
